167f58520d8c95551f0b2dd33b7571a49b3ce949
Thiago Santos <thilego418@gmail.com>
2022-11-17 20:21:33 -0300

    Adicionando .gitignore para não commitar o executável e incluindo, em PIM_IV.c, bibliotecas iniciais que serão úteis para o projeto.

--- PIM_IV.c
+++ PIM_IV.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
 void main() {
-    print("Iniciando repositório!")
+    setlocale(LC_ALL, "Portuguese");
+    printf("Iniciando repositório!");
 }
