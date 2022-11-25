40690ad91ef53d9aa92b18b917e2bf8bfec48a79
Thiago Santos <thilego418@gmail.com>
2022-11-25 00:26:11 -0300

    Pequenas alterações no código para inicar adaptação à nova organização dos arquivos

--- PIM_IV/main.c
+++ PIM_IV/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include "paciente.c"
 
 void menu ();
 void cadastrar();
@@ -12,72 +13,4 @@ int main() {
     menu();
 
     return 0;
-}
-
-void menu () {
-    printf("este é o menu");
-}
-
-void cadastrar() {
-     //Declaração das variaveis
-    char name[80];
-    int idade;
-    char cpf[14];
-    char rua[80];
-    int numero;
-    char bairro[80];
-    char cidade[80];
-    char estado[80];
-    char cep[80];
-    int dataNascimento[3];
-    char comorbidadePaciente[100];
-
-    char lixo[100];
-    char lixoChar;
-
-    //Inicio do formulario
-    printf("Nome: ");
-    scanf("%[^\n]s", name);
-    printf("Idade:");
-    scanf("\n%d", &idade);
-    printf("cpf: ");
-    scanf("%s", cpf);
-    printf("rua: ");
-    scanf("%s", rua);
-    scanf("%s", lixo);
-    printf("número:");
-    scanf("%d", &numero);
-    scanf("%c", &lixoChar);
-    printf("bairro:");
-    scanf("%[^\n]s", bairro);
-    printf("cidade:");
-    scanf("\n%[^\n]s", cidade);
-    printf("estado:");
-    scanf("\n%[^\n]s", estado);
-    printf("cep:");
-    scanf("\n%[^\n]s", cep);
-    printf("data de nascimento - dia:  ");
-    scanf("%d", &dataNascimento[0]);
-    printf("data de nascimento - mes:  ");
-    scanf("%d", &dataNascimento[1]);
-    printf("data de nascimento - ano:  ");
-    scanf("%d", &dataNascimento[2]);
-    printf("comorbidade de Paciente:");
-    scanf("%s", comorbidadePaciente);
-
-
-
-    //Teste para verificar o que está sendo escrito nas variaveis
-    printf("%s\n", name);
-    printf("%d\n", idade);
-    printf("%s\n", cpf);
-    printf("%d\n", rua);
-    printf("%d\n", numero);
-    printf("%d\n", bairro);
-    printf("%s\n", cidade);
-    printf("%d\n", estado);
-    printf("%s\n", cep);
-    printf("%d/%d/%d\n", dataNascimento[0], dataNascimento[1], dataNascimento[2]);
-    printf("%s\n", comorbidadePaciente);
-
 }
--- PIM_IV/paciente.c
+++ PIM_IV/paciente.c
@@ -5,3 +5,83 @@ struct Pessoa {
     char nome[100];
     int idade;
 };
+
+/*
+    Abaixo estão as funções que estavam na main antes
+    Antes de continuá-las, é necessário adaptar às structs
+    Structs necessárias:
+    *Pessoa
+    *Usuário (funcionários)
+    *Paciente
+
+    O arquivo pode ser renomeado mais para frente,
+    dependendo da organização das classes que fizermos
+*/
+
+void menu () {
+    printf("este é o menu");
+}
+
+void cadastrar() {
+     //Declaração das variaveis
+    char name[80];
+    int idade;
+    char cpf[14];
+    char rua[80];
+    int numero;
+    char bairro[80];
+    char cidade[80];
+    char estado[80];
+    char cep[80];
+    int dataNascimento[3];
+    char comorbidadePaciente[100];
+
+    char lixo[100];
+    char lixoChar;
+
+    //Inicio do formulario
+    printf("Nome: ");
+    scanf("%[^\n]s", name);
+    printf("Idade:");
+    scanf("\n%d", &idade);
+    printf("cpf: ");
+    scanf("%s", cpf);
+    printf("rua: ");
+    scanf("%s", rua);
+    scanf("%s", lixo);
+    printf("número:");
+    scanf("%d", &numero);
+    scanf("%c", &lixoChar);
+    printf("bairro:");
+    scanf("%[^\n]s", bairro);
+    printf("cidade:");
+    scanf("\n%[^\n]s", cidade);
+    printf("estado:");
+    scanf("\n%[^\n]s", estado);
+    printf("cep:");
+    scanf("\n%[^\n]s", cep);
+    printf("data de nascimento - dia:  ");
+    scanf("%d", &dataNascimento[0]);
+    printf("data de nascimento - mes:  ");
+    scanf("%d", &dataNascimento[1]);
+    printf("data de nascimento - ano:  ");
+    scanf("%d", &dataNascimento[2]);
+    printf("comorbidade de Paciente:");
+    scanf("%s", comorbidadePaciente);
+
+
+
+    //Teste para verificar o que está sendo escrito nas variaveis
+    printf("%s\n", name);
+    printf("%d\n", idade);
+    printf("%s\n", cpf);
+    printf("%d\n", rua);
+    printf("%d\n", numero);
+    printf("%d\n", bairro);
+    printf("%s\n", cidade);
+    printf("%d\n", estado);
+    printf("%s\n", cep);
+    printf("%d/%d/%d\n", dataNascimento[0], dataNascimento[1], dataNascimento[2]);
+    printf("%s\n", comorbidadePaciente);
+
+}
